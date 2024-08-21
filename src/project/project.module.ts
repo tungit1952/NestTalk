6c2dd6c1be8781994260f676875e5c5d46078662
Your Name <tungit1952@gmail.com>
2024-08-21 16:00:40 +0700

    tung

--- src/project/project.module.ts
+++ src/project/project.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ProjectService } from './project.service';
 import { ProjectController } from './project.controller';
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { User } from "../user/entities/user.entity";
+import { Project } from "./entities/project.entity";
 
 @Module({
+  imports:[
+    TypeOrmModule.forFeature([Project]),
+  ],
   controllers: [ProjectController],
   providers: [ProjectService],
 })
